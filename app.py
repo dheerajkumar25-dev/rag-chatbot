aa1f49146d4e48dd07d11ca9c351fc88af4dfdf7
dheerajkumar25-dev <dheerajkumar.iiit25@gmail.com>
2026-06-14 10:13:10 +0530

    Added Groq integration and Streamlit UI

--- app.py
+++ app.py
@@ -3,13 +3,18 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
+from groq import Groq
 
+# ---------------- GROQ API ----------------
+
+client = Groq(
+    api_key="YOUR_GROQ_API_KEY"
+)
 
 # ---------------- LOAD PDF ----------------
 
 loader = PyPDFLoader("notes.pdf")
 documents = loader.load()
-
 
 # ---------------- SPLIT TEXT ----------------
 
@@ -17,15 +22,14 @@ splitter = RecursiveCharacterTextSplitter(
     chunk_size=500,
     chunk_overlap=50
 )
-docs = splitter.split_documents(documents)
 
+docs = splitter.split_documents(documents)
 
 # ---------------- CREATE EMBEDDINGS ----------------
 
 embeddings = HuggingFaceEmbeddings(
     model_name="sentence-transformers/all-MiniLM-L6-v2"
 )
-
 
 # ---------------- STORE IN FAISS ----------------
 
@@ -34,27 +38,47 @@ vectorstore = FAISS.from_documents(
     embeddings
 )
 
-
 # ---------------- USER QUESTION ----------------
 
 query = input("Ask Question : ")
 
-
 # ---------------- SEARCH RELEVANT CHUNKS ----------------
-
 results = vectorstore.similarity_search(
     query,
     k=2
 )
 
 
-# ---------------- PRINT ANSWER ----------------
+# ---------------- HANDLE NO RESULTS ----------------
 
 if len(results) == 0:
     print("Not found in document")
 
 else:
-    print("\nANSWER:\n")
+    context = ""
     for result in results:
-        print(result.page_content)
+        context += result.page_content + "\n"
+    prompt = f"""
+    Answer the question only from the provided context.
+    Context:
+    {context}
+    Question:
+    {query}
+    If answer is not present in context,
+    say:
+    Not found in document
+    """
+    # ---------------- GROQ RESPONSE ----------------
+
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        model="llama-3.1-8b-instant",
+    )
+    print("\nANSWER:\n")
+    print(chat_completion.choices[0].message.content)
 
